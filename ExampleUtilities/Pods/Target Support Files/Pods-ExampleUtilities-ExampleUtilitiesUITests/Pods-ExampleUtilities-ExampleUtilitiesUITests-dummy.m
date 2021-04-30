84a308beef490a6e4d25832a0d07003503cce985
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-04-30 02:51:43 -0300

    Few fixes

--- ExampleUtilities/Pods/Target Support Files/Pods-ExampleUtilities-ExampleUtilitiesUITests/Pods-ExampleUtilities-ExampleUtilitiesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExampleUtilities_ExampleUtilitiesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ExampleUtilities_ExampleUtilitiesUITests
-@end
